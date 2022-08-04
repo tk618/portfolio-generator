e3d4117494bf24caae73b91fa75b7471de5b7db8
Thai K <tkk3@ymail.com>
2022-08-04 15:58:16 -0400

    final mod9 lesson codes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -126,23 +126,21 @@ Add a New Project
     });
 };
 
-    promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      return generatePage(portfolioData);
-    })
-    .then(pageHTML => {
-      return writeFile(pageHTML);
-    })
-    .then(writeFileResponse => {
-      console.log(writeFileResponse);
-      return copyFile();
-    })
-    .then(copyFileResponse => {
-      console.log(copyFileResponse);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  
-
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
